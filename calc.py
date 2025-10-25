aeb46c1f0581d012be905f9d209415eef8af08dc
Klingri <klingri52@gmail.com>
2025-10-25 20:17:03 +0200

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,24 @@
+x = int(input("Enter first number: "))
+y = int(input("Enter second number: "))
+
+def add(x, y):
+    return x + y
+
+def sub(x, y):
+    return x - y
+
+def mul(x, y):
+    return x * y
+
+def div(x, y):
+    return x / y
+
+print("Please select operation -\n"
+      "1. Add\n"
+      "2. Subtract\n"
+      "3. Multiply\n"
+      "4. Divide\n")
+
+sel = int(input("Select operation (1-4): "))
+
+# test
